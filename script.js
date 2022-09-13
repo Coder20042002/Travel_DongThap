85d6472d881d15cd40da0e863c50b6915baba4b0
Hoai Phuong Tran <phuonghoai090909@gmail.com>
2022-09-13 23:26:20 +0700

    af

---
+++ script.js
@@ -0,0 +1,10 @@
+const buyBtns = document.querySelectorAll(".js-buy-ticket");
+
+function showBuyTickets() {
+    alert("show")
+}
+
+for (const buyBtn of buyBtns) {
+    buyBtn.addEventListener('click', showBuyTickets)
+}
+
